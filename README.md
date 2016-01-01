4da32a76bc5e9fbc53690382ca7a2925fdda4139
Jhonatan Teixeira <jhonatan.teixeira@gmail.com>
2016-01-01 13:23:53 -0200

    first commit adding sonata project

---
+++ README.md
@@ -0,0 +1,4 @@
+blog
+====
+
+A Symfony project created on December 31, 2015, 6:43 pm.
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of User
+ *
+ * @author jhonatan
+ */
+class User
+{
+    //put your code here
+}
